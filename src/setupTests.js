f607427af7b196e3d39375b64b59d9c6adc7bf22
IcyAI <alvinob1515@gmail.com>
2024-04-12 15:09:22 -0400

    Exercise 4.6

--- src/setupTests.js
+++ src/setupTests.js
@@ -19,3 +19,19 @@ console.error = (...args) => {
   const ignoreMessage = MESSAGES_TO_IGNORE.find(message => args.toString().includes(message));
   if (!ignoreMessage) originalError(...args);
 }
+
+
+const MESSAGES_TO_IGNORETwo = [
+  "When testing, code that causes React state updates should be wrapped into >act(...):",
+  "Error:",
+  "The above error occurred"
+  ];
+  
+  const originalErrorTwo = console.error.bind(console.error);
+  
+  console.error = (...args) => {
+  const ignoreMessage = MESSAGES_TO_IGNORETwo.find(message => args.toString().includes(message));
+  if (!ignoreMessage) originalErrorTwo(...args);
+  }
+  
+  jest.setTimeout(30000);
