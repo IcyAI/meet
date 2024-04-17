c578e19b791ebb2874f6cb5b194677fbeb43884b
IcyAI <alvinob1515@gmail.com>
2024-04-17 19:48:40 -0400

    Exercise 4.7

--- src/index.js
+++ src/index.js
@@ -25,4 +25,6 @@ serviceWorkerRegistration.unregister();
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-atatus.notify(new Error('Test Atatus Setup'));
+
+//Atatus testing error
+//atatus.notify(new Error('Test Atatus Setup'));
